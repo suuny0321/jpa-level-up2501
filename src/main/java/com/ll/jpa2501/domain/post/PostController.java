472357c0128ebce198d203bf8bc34b1a76fa549d
suuny0321 <joosun7647@naver.com>
2025-01-07 16:56:07 +0900

    Transactional

--- src/main/java/com/ll/jpa2501/domain/post/PostController.java
+++ src/main/java/com/ll/jpa2501/domain/post/PostController.java
@@ -2,8 +2,11 @@ package com.ll.jpa2501.domain.post;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -12,7 +15,13 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/{id}")
-    public Post showPost(Long id) {
+    public Post showPost( @PathVariable("id") Long id) {
+
         return postService.getPost(id);
+    }
+
+    @GetMapping
+    public List<Post> findByUsername(@PathVariable("username") String username){
+        return  postService.findByUsername(username);
     }
 }
--- src/main/java/com/ll/jpa2501/domain/post/PostService.java
+++ src/main/java/com/ll/jpa2501/domain/post/PostService.java
@@ -1,13 +1,26 @@
 package com.ll.jpa2501.domain.post;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
     private final PostRepository postRepository;
+
     public Post getPost(Long id) {
-        return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Post not found"));
+        return
+                postRepository.findById(id).orElseThrow(
+                        () -> new IllegalArgumentException("Post not found"));
+    }
+
+    public List<Post> findByUsername(String username) {
+        postRepository.findById(1L);
+        postRepository.findByUsername(username);
+        return postRepository.findByUsername(username);
     }
 }
